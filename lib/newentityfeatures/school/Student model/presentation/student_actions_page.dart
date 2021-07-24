47053ee7ec426eee688c15d0f89ab554267b2a1e
facu <facuhannoch@gmail.com>
2021-07-24 17:54:03 -0300

    Fix in fee payment

--- lib/newentityfeatures/school/Student model/presentation/student_actions_page.dart
+++ lib/newentityfeatures/school/Student model/presentation/student_actions_page.dart
@@ -85,6 +85,20 @@ class StudentItemPage extends StatelessWidget {
           CustomActionButton(
             title: "Fee Payment",
             onTap: () {
+              if (sessionTerm.length == 1) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FeePaymentListList(
+                      // userRegistrationModel: userRegistrationModel,
+                      entitytype: entitytype,
+                      entityid: entityid,
+                      cardNum: userRegistrationModel.idCardNum,
+                      sessionTerm: sessionTerm.first,
+                    ),
+                  ),
+                );
+              }
               showDialog(
                 context: context,
                 builder: (context) => AlertDialog(
